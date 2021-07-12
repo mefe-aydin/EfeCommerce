9bc14137cad1529518a686dfb7c465c12a6a022b
mefe-aydin <m.efeaydin@outlook.com>
2021-07-12 15:33:09 +0300

    added signin and register pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter, Route } from "react-router-dom";
 import CartScreen from "./secreens/CartScreen";
 import HomeScreen from "./secreens/HomeScreen";
 import ProductScreen from "./secreens/ProductScreen";
+import SigninScreen from "./secreens/SigninScreen";
+import { signout } from "./actions/userActions";
+import RegisterScreen from "./secreens/RegisterScreen";
 
 function App() {
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
 
   return (
     <BrowserRouter>
@@ -23,15 +32,31 @@ function App() {
             <Link to="/cart">
               Cart
               {cartItems.length > 0 && (
-                <span className="badge">{cartItems.length}</span>
+                <span className="badge"> {cartItems.length} </span>
               )}
             </Link>
-            <Link to="/signin">Sign In</Link>
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} {console.log(userInfo)}
+                  <i className="fa fa-caret-down"></i>
+                </Link>
+                <ul className="dropdown-content">
+                  <Link to="signout" onClick={signoutHandler}>
+                    Sign Out
+                  </Link>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin"> Sign In </Link>
+            )}
           </div>
         </header>
         <main>
           <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/signin" component={SigninScreen}></Route>
+          <Route path="/register" component={RegisterScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center"> All right reserved </footer>
